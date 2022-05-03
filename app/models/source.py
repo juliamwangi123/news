3c5f5a99c8bfb05febfaf5d8b2edb2cab421b268
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-03 22:07:17 +0300

    add models

---
+++ app/models/source.py
@@ -0,0 +1,11 @@
+class Sources:
+    
+    def __init__(self,news,description,urlToImage,publishedAt,content):
+        
+        self.title = news
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+    
+    
