aae1f1be62a1ed4ff3c5d0148633ee51948b8039
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-04 09:12:52 +0300

    add secrect key

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from requests import request
 # initialize the app
 
 app= Flask(__name__)
-
+app.config['SECRET_KEY']='41932c548f4fdfa9f7c743348d71e732'
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
